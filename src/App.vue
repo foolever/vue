03c8e7d2af77418f6e6758dda3db0598488febe7
Jiang <502970070@qq.com>
2017-09-11 12:27:24 +0800

    默认

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import 'src/plugins/jquery.min'
+import 'src/plugins/jquery.fullPage'
+export default {
+  name: 'app',
+  mounted(){
+    $("h1").text("123");
+  }
+}
+</script>
+
+<style>
+ul{
+  list-style: none;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="header">
+    <nav>
+      <ul>
+        <li>
+          <a href="javascript:;">
+            首页
+          </a>
+        </li>
+        <li>
+          <a href="javascript:;">
+            首页
+          </a>
+        </li>
+        <li>
+          <a href="javascript:;">
+            首页
+          </a>
+        </li>
+        <li>
+          <a href="javascript:;">
+            首页
+          </a>
+        </li>
+        <li>
+          <a href="javascript:;">
+            首页
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+  nav{
+    li{
+      float: left;
+    }
+  }
+</style>
